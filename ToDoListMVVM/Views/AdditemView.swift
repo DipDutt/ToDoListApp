8ee60e54947ef9e3459beadc3403b67c7f127314
Dip Dutt <emduttt456@gmail.com>
2024-11-18 19:37:40 +0600

    [Feature] add environment property to dismiss view
    [Feature] create function to validate textfield.

--- ToDoListMVVM/Views/AdditemView.swift
+++ ToDoListMVVM/Views/AdditemView.swift
@@ -11,42 +11,67 @@ struct AdditemView: View {
     
     // MARK: - Properties
     @State var textFieldTitle:String = ""
+    @EnvironmentObject var vm: ItemsViewModel
+    @State var showAlert:Bool = false
+    @Environment(\.dismiss) var dismiss
+    
     
     // MARK: - body
     var body: some View {
-        ScrollView {
-            VStack(spacing: 10) {
-                TextField(text: $textFieldTitle, label: {
-                    Text("type your item")
-                })
-                .padding()
-                .frame(height:50)
-                .background(Color.gray.opacity(0.5))
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                
-                // MARK: - Create action Button to save item
-                Button {
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 10) {
+                    TextField(text: $textFieldTitle, label: {
+                        Text("type your item")
+                    })
+                    .padding()
+                    .frame(height:50)
+                    .background(Color.gray.opacity(0.5))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
                     
-                } label: {
-                    Text("Save")
-                        .foregroundStyle(Color.white)
-                        .frame(height: 50)
-                        .frame(maxWidth: .infinity)
-                        .background(Color.blue)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    // MARK: - Create action Button to save item
+                    Button {
+                        additem()
+                    } label: {
+                        Text("Save")
+                            .foregroundStyle(Color.white)
+                            .frame(height: 50)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                    
                 }
-                
+                .padding()
+                .navigationTitle("Add an item 🖌️")
             }
-            .padding()
-            .navigationTitle("Add an item 🖌️")
+            .scrollDisabled(true)
+            .alert(
+                "An alert is showing ❌",
+                isPresented: $showAlert) {
+                    Button("Ok",role: .destructive) {
+                        
+                    }
+                }
         }
-        .scrollDisabled(true)
         
+    }
+    
+    // MARK: - add items from the textField.
+    func additem() {
+        if textFieldTitle.count >= 3 {
+            vm.addItems(item: textFieldTitle)
+            dismiss()
+        }
+        else {
+            showAlert.toggle()
+        }
     }
 }
 // MARK: - Preview
 #Preview {
-    NavigationStack {
-        AdditemView()
-    }
+    
+    AdditemView()
+        .environmentObject(ItemsViewModel())
+    
 }
