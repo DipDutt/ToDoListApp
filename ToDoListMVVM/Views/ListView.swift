952878bb04afae5ddda7aab0a5fb8349f88eb2ae
Dip Dutt <emduttt456@gmail.com>
2024-11-18 19:35:17 +0600

    [Bug] remove navigationstack from preview in listview
    []

--- ToDoListMVVM/Views/ListView.swift
+++ ToDoListMVVM/Views/ListView.swift
@@ -9,32 +9,35 @@ import SwiftUI
 
 struct ListView: View {
     @EnvironmentObject var vm: ItemsViewModel
+    
     // MARK: - body
     var body: some View {
-        List {
-            ForEach(vm.items) { item in
-                ListRowView(item: item)
-            }
-            .onDelete(perform:vm.deleteItems)
-            .onMove(perform: vm.moveItems)
-        }
-        
-        .listStyle(.plain)
-        .navigationTitle(" ToDo ListItem 📝")
-        .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                EditButton()
-            }
-            ToolbarItem(placement: .topBarTrailing) {
-                NavigationLink {
-                    AdditemView()
-                } label: {
-                    HStack {
-                        Text("Add Item")
-                        Image(systemName: "plus.circle")
-                    }
+        NavigationStack {
+            List {
+                ForEach(vm.items) { item in
+                    ListRowView(item: item)
                 }
-                
+                .onDelete(perform:vm.deleteItems)
+                .onMove(perform: vm.moveItems)
+            }
+            
+            .listStyle(.plain)
+            .navigationTitle(" ToDo ListItem 📝")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink {
+                        AdditemView()
+                    } label: {
+                        HStack {
+                            Text("Add Item")
+                            Image(systemName: "plus.circle")
+                        }
+                    }
+                    
+                }
             }
         }
         
@@ -42,7 +45,7 @@ struct ListView: View {
 }
 // MARK: - Preview
 #Preview {
-    NavigationStack {
-        ListView()
-    }
+    ListView()
+        .environmentObject(ItemsViewModel())
+    
 }
